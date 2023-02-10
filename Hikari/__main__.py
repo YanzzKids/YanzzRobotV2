8482d600487c4eaacb275f94f9385c861f770630
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-11 01:19:03 +0700

    tes

--- Hikari/__main__.py
+++ Hikari/__main__.py
@@ -204,7 +204,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAx0EbLl4TwADV2PmiaOgYotC_VsgB0tVmL9HsDvjAAI7CwACz-YwV8Xs8Z3GYNmwHgQ"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
