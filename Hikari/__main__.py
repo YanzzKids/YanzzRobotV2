3531c5d405e92503452a40a2436a50eb9f1e111f
RexAshh <95988728+Rexashh@users.noreply.github.com>
2023-02-11 13:42:03 +0700

    Update __main__.py

--- Hikari/__main__.py
+++ Hikari/__main__.py
@@ -911,14 +911,13 @@ def main():
                 chat_id=f"@{SUPPORT_CHAT}",
                 photo=START_IMG,
                 caption=f"""
-ㅤ🌻 {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
-
-┏•❅────✧❅✦❅✧────❅•┓
-ㅤ★ **ᴘʏᴛʜᴏɴ :** `{y()}`
-ㅤ★ **ʟɪʙʀᴀʀʏ :** `{telever}`
-ㅤ★ **ᴛᴇʟᴇᴛʜᴏɴ :** `{tlhver}`
-ㅤ★ **ᴩʏʀᴏɢʀᴀᴍ :** `{pyrover}`
-┗•❅────✧❅✦❅✧────❅•┛""",
+ㅤ🌻 {BOT_NAME} ᴀᴋᴜ ʜɪᴅᴜᴘ...
+────────
+ㅤ• **ᴘʏᴛʜᴏɴ :** `{y()}`
+ㅤ• **ʟɪʙʀᴀʀʏ :** `{telever}`
+ㅤ• **ᴛᴇʟᴇᴛʜᴏɴ :** `{tlhver}`
+ㅤ• **ᴩʏʀᴏɢʀᴀᴍ :** `{pyrover}`
+────────""",
                 parse_mode=ParseMode.MARKDOWN,
             )
         except Unauthorized:
