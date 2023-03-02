42fb91e0d1443d02cb480381a944150db709c71a
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-02 20:18:51 +0800

    Update reporting_sql.py

--- Yanzz/modules/sql/reporting_sql.py
+++ Yanzz/modules/sql/reporting_sql.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from sqlalchemy import BigInteger, Boolean, Column, String
 
-from Hikari.modules.sql import BASE, SESSION
+from Yanzz.modules.sql import BASE, SESSION
 
 
 class ReportingUserSettings(BASE):
