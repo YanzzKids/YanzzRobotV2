887d0aa0e1889c6efa17fc8e6012431206538876
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-02 19:50:33 +0800

    Update errors.py

--- Yanzz/utils/errors.py
+++ Yanzz/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from Hikari import OWNER_ID, pbot
+from Yanzz import OWNER_ID, pbot
 
 
 def split_limits(text):
