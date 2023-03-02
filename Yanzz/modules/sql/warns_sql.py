9d12bdd6ebf7c3536bdde16775e5ddf9acdd66ff
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-02 20:21:38 +0800

    Update warns_sql.py

--- Yanzz/modules/sql/warns_sql.py
+++ Yanzz/modules/sql/warns_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText, distinct, func
 from sqlalchemy.dialects import postgresql
 
-from Hikari.modules.sql import BASE, SESSION
+from Yanzz.modules.sql import BASE, SESSION
 
 
 class Warns(BASE):
