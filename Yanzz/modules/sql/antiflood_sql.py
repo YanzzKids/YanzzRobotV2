ab4f92fe6a49aa3cab96064d3f8c93d35448fc23
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-02 20:09:13 +0800

    Update antiflood_sql.py

--- Yanzz/modules/sql/antiflood_sql.py
+++ Yanzz/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import BigInteger, Column, String, UnicodeText
 
-from Hikari.modules.sql import BASE, SESSION
+from Yanzz.modules.sql import BASE, SESSION
 
 DEF_COUNT = 1
 DEF_LIMIT = 0
