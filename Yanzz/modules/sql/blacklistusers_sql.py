c77bd223e26d560d7befce3548a641dfdfb39fd6
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-02 20:10:24 +0800

    Update blacklistusers_sql.py

--- Yanzz/modules/sql/blacklistusers_sql.py
+++ Yanzz/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from Hikari.modules.sql import BASE, SESSION
+from Yanzz.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
