6120900a881cfdbd2cca3cfcc9cf10fe89667a6c
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-03 17:36:18 +0800

    Update users.py

--- Yanzz/modules/users.py
+++ Yanzz/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import Hikari.modules.sql.users_sql as sql
-from Hikari import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from Hikari.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from Hikari.modules.sql.users_sql import get_all_users
+import Yanzz.modules.sql.users_sql as sql
+from Yanzz import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from Yanzz.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from Yanzz.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
