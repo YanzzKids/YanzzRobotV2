f70e252bf84ef2922a081ec7ab692590fd62f4c5
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-03 17:43:49 +0800

    Update tagall.py

--- Yanzz/modules/tagall.py
+++ Yanzz/modules/tagall.py
@@ -5,7 +5,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from Hikari import telethn as client
+from Yanzz import telethn as client
 
 spam_chats = []
 
