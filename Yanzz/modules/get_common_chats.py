665e7823cf4ac456ce9c6127601008f57cff3dc2
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-03 20:41:51 +0800

    Update get_common_chats.py

--- Yanzz/modules/get_common_chats.py
+++ Yanzz/modules/get_common_chats.py
@@ -6,9 +6,9 @@ from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 
-from Hikari import OWNER_ID, dispatcher
-from Hikari.modules.helper_funcs.extraction import extract_user
-from Hikari.modules.sql.users_sql import get_user_com_chats
+from Yanzz import OWNER_ID, dispatcher
+from Yanzz.modules.helper_funcs.extraction import extract_user
+from Yanzz.modules.sql.users_sql import get_user_com_chats
 
 
 @run_async
