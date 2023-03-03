7375d43918bac76cd601e4ebc9abd18bf3142aa2
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-03 20:31:36 +0800

    Update google.py

--- Yanzz/modules/google.py
+++ Yanzz/modules/google.py
@@ -12,8 +12,8 @@ from bs4 import BeautifulSoup
 from PIL import Image
 from search_engine_parser import GoogleSearch
 
-from Hikari import telethn as tbot
-from Hikari.events import register
+from Yanzz import telethn as tbot
+from Yanzz.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 11; SM-M017F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
