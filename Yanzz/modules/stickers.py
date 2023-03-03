8145bc4288eda16f273aa27dddad322752a58912
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-03 17:45:39 +0800

    Update stickers.py

--- Yanzz/modules/stickers.py
+++ Yanzz/modules/stickers.py
@@ -16,8 +16,8 @@ from telegram import (
 from telegram.ext import CallbackContext, run_async
 from telegram.utils.helpers import mention_html
 
-from Hikari import dispatcher
-from Hikari.modules.disable import DisableAbleCommandHandler
+from Yanzz import dispatcher
+from Yanzz.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
