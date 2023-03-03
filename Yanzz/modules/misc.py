7ef7fa7f09dc6a6e8c121bb91a268d1815deec86
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-03 20:12:16 +0800

    Update misc.py

--- Yanzz/modules/misc.py
+++ Yanzz/modules/misc.py
@@ -2,9 +2,9 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 from telegram.ext import CallbackContext, CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 
-from Hikari import dispatcher
-from Hikari.modules.disable import DisableAbleCommandHandler
-from Hikari.modules.helper_funcs.chat_status import user_admin
+from Yanzz import dispatcher
+from Yanzz.modules.disable import DisableAbleCommandHandler
+from Yanzz.modules.helper_funcs.chat_status import user_admin
 
 MARKDOWN_HELP = f"""
 Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
