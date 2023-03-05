1c968bca52f668d52697f5410b88a39029cb9b0a
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-05 21:04:07 +0800

    Update __main__.py

--- Yanzz/__main__.py
+++ Yanzz/__main__.py
@@ -631,6 +631,7 @@ def donasi_about_callback(update, context):
             ),
          )
  
+ 
 @run_async
 def Source_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
