c92453fb279b746a7e70a14b34cd9c756fda8e92
YanzzKids <117140890+YanzzKids@users.noreply.github.com>
2023-03-06 07:37:08 +0800

    Update mongo.py

--- Yanzz/utils/mongo.py
+++ Yanzz/utils/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from Hikari import MONGO_DB_URI
+from Yanzz import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.Yanzz
